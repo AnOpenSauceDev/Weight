1f127945dae84f8a1f43054713b15207ea6292c5
AnOpenSauceDev <119765865+AnOpenSauceDev@users.noreply.github.com>
2023-02-26 10:32:12 +1000

    some work is being done for once here

--- src/main/java/an/opensauce/armourweight/Armourweight.java
+++ src/main/java/an/opensauce/armourweight/Armourweight.java
@@ -1,15 +1,26 @@
 package an.opensauce.armourweight;
 
 import an.opensauce.armourweight.api.armourType;
+import an.opensauce.armourweight.commands.CalculateWeightCommand;
 import an.opensauce.armourweight.util.Config;
 import an.opensauce.armourweight.util.defaultdefs.VanillaDefs;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+
+import static net.minecraft.server.command.CommandManager.literal;
 
 public class Armourweight implements ModInitializer {
 
@@ -18,8 +29,11 @@ public class Armourweight implements ModInitializer {
     public static Logger MainLogger = LoggerFactory.getLogger(ModId);
     @Override
     public void onInitialize() {
-        MainLogger.info("armour weight loaded!");
+        MainLogger.info("Armour Weight has loaded!");
         VanillaDefs.Register();
         AutoConfig.register(Config.class, JanksonConfigSerializer::new);
+        CalculateWeightCommand command = new CalculateWeightCommand();
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> literal("CalculateWeight")
+                        .executes(new CalculateWeightCommand()));
     }
 }
---
+++ src/main/java/an/opensauce/armourweight/commands/CalculateWeightCommand.java
@@ -0,0 +1,37 @@
+package an.opensauce.armourweight.commands;
+
+import an.opensauce.armourweight.util.WeightUtil;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.command.GiveCommand;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.command.TeleportCommand;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+
+import java.util.Collection;
+
+public class CalculateWeightCommand implements Command<ServerCommandSource> /*implements Command<ServerCommandSource> */{
+
+    public void register(){
+
+    }
+
+    @Override
+    public int run(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        ServerCommandSource source = context.getSource();
+
+        if(source.getPlayer() != null){ // if our sender exists
+            PlayerEntity player = source.getPlayer();
+            float value = WeightUtil.CalculateWeight(player);
+            player.sendMessage(Text.of("DEBUG: weight = " + value));
+            return (int) (value * 10); // a weight of "3.5" will return as "35"
+        }else { // if the player doesn't exist (like this being run from the console without "execute as <Player>")
+            return 0;
+        }
+    }
+
+}
